db72cc62d43ec3c38b222afb2bbfdda955bb7aac
mahfudzihanif3001 <hanifmahfudzi3001@gmail.com>
2025-12-17 23:07:43 +0700

    re-setup client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router";
+import { GameProvider } from "./contexts/GameContext";
+import LoginScreen from "./components/screens/LoginScreen";
+import LobbyScreen from "./components/screens/LobbyScreen";
+import WaitingRoom from "./components/screens/WaitingRoom";
+import RacingScreen from "./components/screens/RacingScreen";
+import FinishedScreen from "./components/screens/FinishedScreen";
+import { RPGAlertContainer } from "./components/ui/RPGAlert";
+import SoundControl from "./components/ui/SoundControl";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" />
-      </Routes>
+      <GameProvider>
+        <RPGAlertContainer />
+        <SoundControl />
+        <Routes>
+          <Route path="/" element={<LoginScreen />} />
+          <Route path="/lobby" element={<LobbyScreen />} />
+          <Route path="/waiting" element={<WaitingRoom />} />
+          <Route path="/racing" element={<RacingScreen />} />
+          <Route path="/finished" element={<FinishedScreen />} />
+        </Routes>
+      </GameProvider>
     </BrowserRouter>
   );
 }
