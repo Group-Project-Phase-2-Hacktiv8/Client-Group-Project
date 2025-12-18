237a985b57d273b8e17d30b8884f4b8aae20b7e3
MuhammadFarrasDaffa <daffamfd27@gmail.com>
2025-12-18 09:59:23 +0700

    feat : setup FIREBASE

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -2,7 +2,7 @@
 import { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 
-const SERVER_URL = 'http://localhost:5000';
+const SERVER_URL = 'https://myproject.fardaf.web.id';
 
 export function useSocket() {
     const socketRef = useRef(null);
